dd5e1482f95faaaa2a9d7d1e436c82e8ea7579b0
Max <Bilanych.max98009@gmail.com>
2021-08-05 11:01:28 +0300

    fix menu noscroll

--- dist/script.js
+++ dist/script.js
@@ -8213,6 +8213,12 @@ const openMenu = (selectorMenu, selectorBtn, selectorListItem) => {
   btn.addEventListener('click', () => {
     menu.classList.toggle('header_active');
     listItem.classList.toggle('header-nav__list_active');
+
+    if (menu.classList.contains('header_active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
   });
 };
 
--- src/js/modules/openMenu.js
+++ src/js/modules/openMenu.js
@@ -6,6 +6,11 @@ const openMenu = (selectorMenu, selectorBtn, selectorListItem) => {
   btn.addEventListener('click', () => {
     menu.classList.toggle('header_active');
     listItem.classList.toggle('header-nav__list_active');
+    if (menu.classList.contains('header_active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
   });
 
 };
