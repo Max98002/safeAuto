30fad05966ea1b31749b2b931fa68a4f45ee7f33
Max <Bilanych.max98009@gmail.com>
2021-08-23 14:02:35 +0300

    add detect mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
+import MobileDetect from 'mobile-detect';
 import fixHeader from './modules/fixHeader';
 import openMenu from './modules/openMenu';
 import openQuestion from './modules/openQuestion';
@@ -7,6 +8,18 @@ import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
+
+  let md = new MobileDetect(window.navigator.userAgent);
+
+  console.log(md.mobile());
+
+  const btnPosition = document.querySelector('.button_position');
+
+  if (md.mobile() === 'iPhone') {
+    btnPosition.classList.toggle('button_position_iPhone');
+  } else {
+    console.log('not iPhone');
+  }
 
   openMenu('.header', '.burger', '.header-nav__list');
   openQuestion('.question-header');
