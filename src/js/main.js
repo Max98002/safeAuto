abd7b2a2c618750402fafb4683ca53dea1717e79
Max <Bilanych.max98009@gmail.com>
2021-08-25 11:12:25 +0300

    fix all bag

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
-import MobileDetect from 'mobile-detect';
+// import MobileDetect from 'mobile-detect';
 import fixHeader from './modules/fixHeader';
 import openMenu from './modules/openMenu';
 import openQuestion from './modules/openQuestion';
@@ -15,17 +15,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
   tabs('.tabs__header', '.tabs-header__btn', '.tabs-content__item', 'tabs-header__btn_active');
 
-  try {
-    let md = new MobileDetect(window.navigator.userAgent);
-    const btnPosition = document.querySelector('.button_position');
+  // try {
+  //   let md = new MobileDetect(window.navigator.userAgent);
+  //   const btnPosition = document.querySelector('.button_position');
 
-    if (md.mobile() === 'iPhone') {
-      btnPosition.classList.toggle('button_position_iPhone');
-    } else {
-      console.log('not iPhone');
-    }
-  } catch (error) {
+  //   if (md.mobile() === 'iPhone') {
+  //     btnPosition.classList.toggle('button_position_iPhone');
+  //   } else {
+  //     console.log('not iPhone');
+  //   }
+  // } catch (error) {
 
-  }
+  // }
 
 });
